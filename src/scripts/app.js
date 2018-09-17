b7ce34e0f6790f98d85685fd4d5afe4e5b708c62
Jesse <jesseca.melise@live.com>
2018-09-17 00:27:44 -0400

    added a bunch of styles and changed the header NAV

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,28 @@
-const hello = 'World';
-console.log(hello);
+const app = []
+
+app.events = () => {
+    // SMOOTH SCROLL 
+    // HEADER NAV
+    $('.nav__listItem--cool h6').on('click', function(){
+        console.log('hi')
+        $(this).next().toggleClass('show');
+    }) 
+    // CONTACT FORM
+    $('.fa-envelope-square, .nav__listItem--contact').on('click', function (e) {
+        e.preventDefault();
+        $('.container--contact').addClass('show');
+    });
+
+    $('#close').on('click', function (e) {
+        e.preventDefault();
+        $('.container--contact').removeClass('show')
+    })
+}
+
+app.init = function () {
+   app.events()
+}
+
+$(function(){
+    app.init();
+});
