b4d8c6aab4d9384d415ffa64c868c43b5ca7a0c0
Jesse <jesseca.melise@live.com>
2018-09-18 19:25:00 -0400

    changed some styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,30 +1,71 @@
 const app = []
+const menuTab = $('.nav__container--header .nav__listItem')
 
-app.clickEvents = () => {
+app.bagels = () => {
+    const pressed = [];
+    const code = "bensbagels";
+    // let frameCount = 0;
+
+    const finishbagels = () => {
+        let bagel = document.getElementById("bagel-container");
+        bagel.className = "hide";
+    }
+
+    const trigger = () => {
+        setTimeout(
+            function () {
+                finishbagels();
+            }, 10000);
+    }
+
+    const startbagels = () => {
+        let bagel = document.getElementById("bagel-container");
+        bagel.className = "show";
+        trigger();
+
+    }
+
+    window.addEventListener("keyup", function (e) {
+        pressed.push(e.key);
+        pressed.splice(-code.length - 1, pressed.length - code.length);
+
+        if (pressed.join("").includes(code)) {
+            startbagels();
+        }
+    });
+}
+
+app.styles = () => {
     // SMOOTH SCROLL 
     $('a').smoothScroll({
-        offset:50,
+        offset: 50,
     });
+    $('.nav__container--sub').hide();
 
     // HEADER NAV
+    $('.nav__container--header .nav__listItem').addClass('animated bounceInDown')
+    setTimeout(() => {
+        menuTab.hide('slow')
+    }, 3000)
+}
+
+app.clickEvents = () => {
+    // HEADER NAV
     $('.nav__listItem--cool h6').on('click', function(){
-        $(this).next().toggleClass('show');
+        menuTab.toggle(); 
+        setTimeout(() => {
+            menuTab.hide('slow')
+        }, 5000)
     }) 
-
-    // CONTACT FORM
-    $('.fa-envelope-square, .nav__listItem--contact').on('click', function (e) {
-        e.preventDefault();
-        $('.container--contact').addClass('show');
-    });
-
-    $('#close').on('click', function (e) {
-        e.preventDefault();
-        $('.container--contact').removeClass('show')
+    $('.nav__listItem--contact').on('mouseover', function() {
+        $('.nav__container--sub').show('fast')
     })
 }
 
 app.init = function () {
-   app.clickEvents()
+    app.clickEvents();
+    app.styles();
+    app.bagels();
 }
 
 $(function(){
